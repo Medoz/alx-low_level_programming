11e844b377c52c0ec380d7a48b67b61aa0a5fc23
Medoz <medozfawzy@gmail.com>
2023-03-21 18:09:01 -0700

    f2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,23 +3,21 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * Return: 1 for positive m , -1 for negative or 0 for else
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		return (1);
-	} else if (n == 0)
-}
-		_putchar(48);
-		return (0);
+	  _putchar(43);
+	  return (1);
 	} else if (n < 0)
+        {
+	  _putchar(45);
+	  return (-1);
+	} else
 	{
-		_putchar('-');
+		_putchar(48);
 	}
-		return (-1);
+		return (0);
 }
